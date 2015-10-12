05083e3f88ed9651e9ef46bb73f4ab27fed8844e
Clemens Rudert <clemens.rudert@bl.ch>
2015-10-12 16:01:38 +0200

    add m to n handling to the update und create methods

--- lib/rest.py
+++ lib/rest.py
@@ -595,6 +595,33 @@ class Rest(object):
         """
         return self.model.description(self.dictionary)
 
+    def m_to_n_handling(self, key, value):
+        """
+        Small helper method. It checks if the passed key is defined as an m_to_n relation ship to other tables. If
+        it is: The method gets the corresponding objects from database by the passed (comma seperated) id's and return
+         them as a list of such objects.
+
+        :param key: The name of the column which should be checked
+        :type key: str
+        :param value: The effective value which is intended to be set to the column
+        :type value:
+        :return: The found results of corresponding datasets or False if the checked column was not a m_to_n one
+        :rtype: bool or list
+        """
+        for column in self.model.description(self.dictionary).get('columns'):
+            # found the column which is m:n
+            if column.get('is_m_to_n') and key == column.get('column_name'):
+                value_list = str(value).split(',')
+                bound_model = getattr(self.model, key).argument
+                pk_name = bound_model.description(self.dictionary).get('pk_name')
+                pk_column = getattr(bound_model, pk_name)
+                relation_list = []
+                for identifier in value_list:
+                    result = self.session.query(bound_model).filter(pk_column == identifier).one()
+                    relation_list.append(result)
+                return relation_list
+        return False
+
     def create(self, request):
         """
         Creates a new record in the corresponding database table.
@@ -615,6 +642,9 @@ class Rest(object):
                 for key, value in data.iteritems():
                     if key == 'geom':
                         value = WKBSpatialElement(buffer(wkt.loads(value).wkb), srid=2056)
+                    m_to_n = self.m_to_n_handling(key, value)
+                    if m_to_n:
+                        value = m_to_n
                     setattr(new_record, key, value)
                 session.add(new_record)
                 session.flush()
@@ -653,6 +683,9 @@ class Rest(object):
                 for key, value in data.iteritems():
                     if key == 'geom':
                         value = WKBSpatialElement(buffer(wkt.loads(value).wkb), srid=2056)
+                    m_to_n = self.m_to_n_handling(key, value)
+                    if m_to_n:
+                        value = m_to_n
                     setattr(element, key, value)
                 session.flush()
                 request.response.status_int = 202
