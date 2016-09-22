08fd1604c4902941b5703744c8882e58137ede9d
Clemens Rudert <clemens.rudert@bl.ch>
2016-09-22 14:26:27 +0200

    implement a central request gathering provider to distribute the incoming requests to the right api

--- pyramid_rest/views/__init__.py
+++ pyramid_rest/views/__init__.py
@@ -14,6 +14,8 @@
 # 
 # The above copyright notice and this permission notice shall be included in all copies or substantial
 # portions of the Software.
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.view import view_config
 
 __author__ = 'Clemens Rudert'
 __create_date__ = '29.07.2015'
@@ -21,3 +23,57 @@ __create_date__ = '29.07.2015'
 
 def doc(request):
     return {}
+
+
+class RestProxy(object):
+    read_method = 'GET'
+    create_method = 'POST'
+    update_method = 'PUT'
+    delete_method = 'DELETE'
+
+    def __init__(self, request):
+        self.request = request
+        api_name = request.matchdict['api_name']
+        self.api = request.registry.pyramid_rest_apis.get(api_name)
+        if self.api is None:
+            raise HTTPNotFound(detail='API with name {name} is not defined.'.format(name=api_name))
+
+    @view_config(
+        route_name='read',
+        request_method=read_method,
+        permission=None  # 'read_json' if self.with_read_permission else None
+    )
+    def read(self):
+        return self.api.read(self.request)
+
+    @view_config(
+        route_name='show',
+        request_method=read_method,
+        permission=None  # 'read_json' if self.with_read_permission else None
+    )
+    def show(self):
+        return self.api.show(self.request)
+
+    @view_config(
+        route_name='create',
+        request_method=create_method,
+        permission=None  # 'read_json' if self.with_read_permission else None
+    )
+    def create(self):
+        return self.api.create(self.request)
+
+    @view_config(
+        route_name='delete',
+        request_method=delete_method,
+        permission=None  # 'read_json' if self.with_read_permission else None
+    )
+    def delete(self):
+        return self.api.delete(self.request)
+
+    @view_config(
+        route_name='update',
+        request_method=update_method,
+        permission=None  # 'read_json' if self.with_read_permission else None
+    )
+    def update(self):
+        return self.api.update(self.request)
