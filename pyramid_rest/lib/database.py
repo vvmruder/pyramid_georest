e04c3c6e2b5e3b3fcf625989fb196c8ad7f65b71
Clemens Rudert <clemens.rudert@bl.ch>
2016-10-07 13:02:46 +0200

    add the docstring

--- pyramid_rest/lib/database.py
+++ pyramid_rest/lib/database.py
@@ -26,6 +26,14 @@ __create_date__ = '08.09.16'
 class Connection:
 
     def __init__(self, url):
+        """
+        Simple wrapper class to have all connection specific stuff in one simple accessible place.
+
+        :param url: The connection string which is used to let the api connect with the desired database.
+        It must have the form as described here:
+        http://docs.sqlalchemy.org/en/latest/core/engines.html
+        :type url: str
+        """
         self.url = url
         if 'cx_oracle' in self.url:
             self.engine = create_engine(self.url, echo=True, pool_size=1, coerce_to_unicode=True)
